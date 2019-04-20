2b1a2bcf417ca899bdcfa55a7fe4bbe779ba9efd
katus <katus98@users.noreply.github.com>
2019-04-20 22:48:33 +0800

    Update Instructions.js
    增加对撮合表和成交表的方法

--- StockTradingSystem/sqlClasses/Instructions.js
+++ StockTradingSystem/sqlClasses/Instructions.js
@@ -15,7 +15,7 @@ function Instructions() {
     实现功能：插入交易指令
     传入参数：tradeType（'sell', 'buy'）、personid（整数）、stockid（字符串）、shares（整数）、pricePer（浮点数）、回调函数
     回调参数：true（插入成功）, false（插入失败）
-    编程者：孙克染（demo）、陈玮烨
+    编程者：孙克染、陈玮烨
     * */
     this.addInstructions = function (tradeType, personid, stockid, shares, pricePer, callback) {
         let addSql = "INSERT INTO ";
@@ -36,6 +36,49 @@ function Instructions() {
             //console.log('INSERT ID:', result);
             callback(true);
             //matchOnInsertion(istID, tradeType, shares, pricePer, code);
+        });
+    };
+    /*
+    方法名称：addMatchs
+    实现功能：插入撮合记录
+    传入参数：askId、bidId、shares（整数）、askPrice（浮点数）、bidPrice（浮点数）、matchPrice（浮点数）、stockId（字符串）、回调函数
+    回调参数：true（插入成功）, false（插入失败）
+    编程者：孙克染
+    * */
+    this.addMatchs = function (askId, bidId, shares, askPrice, bidPrice, matchPrice, stockId, callback) {
+        let addSql = "INSERT INTO matchs(askid, bidid, shares, askprice, bidprice, matchprice, code) VALUES(?,?,?,?,?,?,?)";
+        let addSqlParams = [askId, bidId, shares, askPrice, bidPrice, matchPrice, stockId];
+        dbConnection.query(addSql, addSqlParams, function (err, result) {
+            if (err) {
+                console.log('[INSERT ERROR] - ', err.message);
+                callback(false);
+                return;
+            }
+            callback(true);
+        });
+    };
+    /*
+    方法名称：addDeals
+    实现功能：插入撮合记录
+    传入参数：askId、bidId、shares（整数）、askPrice（浮点数）、bidPrice（浮点数）、matchPrice（浮点数）、stockId（字符串）、回调函数
+    回调参数：true（插入成功）, false（插入失败）
+    编程者：孙克染
+    * */
+    this.addDeals = function (tradeType, instructionId, shares, sharesDealed, price, stockId, callback) {
+        let addSql = "INSERT INTO ";
+        if (tradeType === "sell") {
+            addSql += 'dealsask(id, shares, sharesdealed, price, code) VALUES(?,?,?,?,?)';
+        } else {
+            addSql += 'dealsbid(id, shares, sharesdealed, price, code) VALUES(?,?,?,?,?)';
+        }
+        let addSqlParams = [instructionId, shares, sharesDealed, price, stockId];
+        dbConnection.query(addSql, addSqlParams, function (err, result) {
+            if (err) {
+                console.log('[INSERT ERROR] - ', err.message);
+                callback(false);
+                return;
+            }
+            callback(true);
         });
     };
     /****更新方法****/
@@ -60,13 +103,21 @@ function Instructions() {
                 callback(false);
                 return;
             }
-            Instructions.updateInstructionsStatus(function (result) {
+            Instructions.completeInstructions(function (result) {
                 console.log(result);
             });
             callback(true);
         });
     };
-    Instructions.updateInstructionsStatus = function (callback) {
+    /*
+    方法名称：completeInstructions
+    实现功能：完成指令状态更新
+    传入参数：回调函数
+    回调参数：true（更新成功）, false（更新失败）
+    编程者：孙克染
+    备注：类成员函数，仅限于类内调用
+    * */
+    Instructions.completeInstructions = function (callback) {
         let modSql1 = "UPDATE asks SET status = 'complete' WHERE shares2trade = 0";
         let modSql2 = "UPDATE bids SET status = 'complete' WHERE shares2trade = 0";
         dbConnection.query(modSql1, function (err, result) {
