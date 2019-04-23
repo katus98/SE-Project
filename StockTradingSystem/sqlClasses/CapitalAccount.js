65dc7fa882dd2fc62cb70349a2079cf57c913008
katus <katus98@users.noreply.github.com>
2019-04-23 11:27:17 +0800

    Update CapitalAccount.js

--- StockTradingSystem/sqlClasses/CapitalAccount.js
+++ StockTradingSystem/sqlClasses/CapitalAccount.js
@@ -34,13 +34,14 @@ function CapitalAccount() {
     /*
     方法名称：getCapitalAccountStateByCapitalAccountId
     实现功能：通过资金账户ID获取资金账户状态
-    传入参数：capitalAccountId（整数或者数字字符串）、回调函数
+    传入参数：capitalAccountId（整数）、回调函数
     回调参数：字符串：'normal', 'frozen', 'logout', 'notFound'
     编程者：孙克染
     * */
     this.getCapitalAccountStateByCapitalAccountId = function (capitalAccountId, callback) {
-        let getSql = "SELECT capitalaccountstate FROM capitalaccount WHERE capitalaccountid = " + capitalAccountId;
-        dbConnection.query(getSql, function (err, result) {
+        let getSql = "SELECT capitalaccountstate FROM capitalaccount WHERE capitalaccountid = ?";
+        let getSqlParams = [capitalAccountId];
+        dbConnection.query(getSql, getSqlParams, function (err, result) {
             if (err) {
                 console.log("ERROR: CapitalAccount: getCapitalAccountStateByCapitalAccountId");
                 console.log('[SELECT ERROR] - ', err.message);
@@ -121,8 +122,8 @@ function CapitalAccount() {
     备注：调用时需要先判断返回的结果是否是'notFound'
     * */
     this.getSecuritiesAccountIdByCapitalAccountId = function (capitalAccountId, callback) {
-        let getSql = "SELECT relatedsecuritiesaccountid FROM capitalaccount WHERE capitalaccountid = ";
-        let getSqlParams = [capitalAccountId];
+        let getSql = "SELECT relatedsecuritiesaccountid FROM capitalaccount WHERE capitalaccountid = ?";
+        let getSqlParams = [parseInt(capitalAccountId)];
         dbConnection.query(getSql, getSqlParams, function (err, result) {
             if (err) {
                 console.log("ERROR: CapitalAccount: getSecuritiesAccountIdByCapitalAccountId");
